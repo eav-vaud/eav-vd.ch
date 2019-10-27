14649351a573a3d0b984b02559601c3ae5ff48e6
Nelson Reitz <nelson@antistatique.net>
2019-10-27 15:43:11 +0100

    Remove bio

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
+    title: `EAV Vaud`,
+    author: `EAV Vaud`,
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
-    social: {
-      twitter: `kylemathews`,
-    },
   },
   plugins: [
     `gatsby-plugin-netlify-cms`,
