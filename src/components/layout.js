17e952e8548836f6d65044a11d020798aaa8675a
Nelson Reitz <nelson@antistatique.net>
2019-12-12 16:21:37 +0100

    Remove footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,23 +1,18 @@
 import React from "react"
 import { ThemeProvider } from "@chakra-ui/core"
 import eavTheme from "../utils/theme"
-import { Box, Flex } from "@chakra-ui/core"
+import { Box } from "@chakra-ui/core"
 
 import Header from "./header.js"
 
 const Layout = ({ title, children }) => (
   <ThemeProvider theme={eavTheme}>
-    <Flex maxW="720px" minH="100vh" direction="column" px="4" mx="auto">
+    <Box maxW="720px" px="4" mx="auto">
       <Header title={title} />
-      <Box as="main" flexGrow="1">
+      <main>
         {children}
-      </Box>
-      <Box as="footer" py="6">
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.org">Gatsby</a>
-      </Box>
-    </Flex>
+      </main>
+    </Box>
   </ThemeProvider>
 )
 
