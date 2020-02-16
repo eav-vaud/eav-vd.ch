e2225f837ac5939c5afa47b1191f2b1eba92ba66
Nelson Reitz <nelson@antistatique.net>
2020-02-16 10:41:11 +0100

    Query news page from prismic

--- src/pages/actualites.js
+++ src/pages/actualites.js
@@ -7,16 +7,22 @@ import { Stack } from "@chakra-ui/core"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import PageHeader from "../components/page-header"
+import RichText from "../components/rich-text"
 import BlogPostTeaser from "../components/blog-post-teaser"
 
 const BlogIndex = ({ data }) => {
   const siteTitle = data.site.siteMetadata.title
   const posts = data.prismic.allBlog_posts.edges
+  const doc = data.prismic.allNews_pages.edges.slice(0, 1).pop()
+  if (!doc) return null
 
   return (
     <Layout title={siteTitle}>
-      <SEO title="Actualités" />
-      <PageHeader title="Actualités" />
+      <SEO title={PrismicText.asText(doc.node.title)} />
+      <PageHeader title={PrismicText.asText(doc.node.title)} />
+      {doc.node.body && (
+        <RichText mt="8">{PrismicText.render(doc.node.body)}</RichText>
+      )}
       <Stack spacing={16} mt="8">
         {posts.map(({ node }) => (
           <BlogPostTeaser
@@ -41,6 +47,14 @@ export const pageQuery = graphql`
       }
     }
     prismic {
+      allNews_pages(uid: "actualites") {
+        edges {
+          node {
+            title
+            body
+          }
+        }
+      }
       allBlog_posts {
         edges {
           node {
