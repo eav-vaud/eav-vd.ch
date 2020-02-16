f9fa803c1d000667a87310eacec9c6a05f5e8edf
Nelson Reitz <nelson@antistatique.net>
2020-02-16 11:46:36 +0100

    Remove netlify-cms

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,12 +35,6 @@ module.exports = {
       },
     },
     {
-      resolve: 'gatsby-plugin-netlify-cms',
-      options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
-    },
-    {
       resolve: 'gatsby-source-prismic-graphql',
       options: {
         repositoryName: 'eav-vd',
--- src/cms/preview-templates/blog-post-preview.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-// import { BlogPostTemplate } from "../../templates/blog-post"
-
-const BlogPostPreview = props => (
-  <div>Test</div>
-)
-
-export default BlogPostPreview
