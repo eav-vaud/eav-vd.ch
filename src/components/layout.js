ee8c66197f77ba4b3cbba92db251cccd1b14b056
Nelson Reitz <nelson@antistatique.net>
2020-03-28 18:16:40 +0100

    Add CSS reset

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,21 +1,24 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { ThemeProvider } from "@chakra-ui/core"
+import { ThemeProvider, CSSReset } from "@chakra-ui/core"
 import eavTheme from "../utils/theme"
 import { Grid, Box } from "@chakra-ui/core"
-import 'typeface-fira-sans'
+import "typeface-fira-sans"
 
 import Header from "./header.js"
 
 const Layout = ({ title, children }) => (
   <ThemeProvider theme={eavTheme}>
+    <CSSReset />
     <Grid
       fontFamily="body"
       templateColumns="minmax(1rem, 1fr) minmax(auto, 60em) minmax(1rem, 1fr)"
       pb="48"
     >
       <Box as={Header} gridColumn="2" title={title} />
-      <Box as="main" gridColumn="2">{children}</Box>
+      <Box as="main" gridColumn="2">
+        {children}
+      </Box>
     </Grid>
   </ThemeProvider>
 )
